69688e3d4429d2c544275c26d0ac125fa8819b57
Juan Manuel Hidalgo <juanma06@gmail.com>
2021-12-01 21:10:41 +0100

    Fixes typo

--- src/modules/transfer/types.ts
+++ src/modules/transfer/types.ts
@@ -19,7 +19,7 @@ export type TransferModal = {
 }
 
 export type TransfersState = {
-  transfers: Transfer[] // storing an array of Transfers because I thought it would be nice to show a history of the toke transfers if I have some time.
+  transfers: Transfer[] // storing an array of Transfers because I thought it would be nice to show a history of the token transfers if I have some time.
   modal: TransferModal
 }
 
